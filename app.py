e512de1fafbaaf4aa82eacac93ce983a2035d599
andriydread <andriy.dread@gmail.com>
2022-01-31 16:36:43 +0200

    Modified texting system

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from shutil import unregister_unpack_format
 from flask import Flask, render_template, request, redirect
 import os
 import psycopg2
@@ -91,6 +92,7 @@ def postgres_profiles(method, login=None, password=None, id=None):
 
 
 
+
 # Func to connect to SESSIONS table
 
 def postgres_sessions(method, id=None, token=None):
@@ -116,7 +118,6 @@ def postgres_sessions(method, id=None, token=None):
             return cursor.fetchone()
         
 
-
     except Exception as ex:
         print('Error occured', ex)
     
@@ -127,22 +128,23 @@ def postgres_sessions(method, id=None, token=None):
 
 
 
+
 # Func to connect to messages table
 
-def postgres_messages(method, user_id_to=None, user_id_from=None, text=None):
+def postgres_messages(method, user_id_to=None, user_id_from=None, text=None, token=None):
     psql = psycopg2.connect(host=os.environ['POST_HOST'], user=os.environ['POST_USER'], password=os.environ['POST_PASSWORD'], database=os.environ['POST_DB'])
     try: 
         psql.autocommit = True
         cursor = psql.cursor()
 
         cursor.execute(
-            """CREATE TABLE IF NOT EXISTS message(id SERIAL PRIMARY KEY, user_id_from BIGINT NOT NULL, user_id_to BIGINT NOT NULL, text TEXT);"""
+            """CREATE TABLE IF NOT EXISTS message(id SERIAL PRIMARY KEY, user_id_from BIGINT NOT NULL, user_id_to BIGINT NOT NULL, text TEXT, token varchar(50));"""
         )
 
 
         if method == 'send_text':
             cursor.execute(
-                """INSERT INTO message(user_id_from, user_id_to, text) VALUES(%s, %s, %s);""", (user_id_from, user_id_to, text,)
+                """INSERT INTO message(user_id_from, user_id_to, text, token) VALUES(%s, %s, %s, %s);""", (user_id_from, user_id_to, text, token)
             )
 
         elif method == 'get_received_messages':
@@ -157,7 +159,13 @@ def postgres_messages(method, user_id_to=None, user_id_from=None, text=None):
             )
             return cursor.fetchall()
 
+        elif method == 'get_messages_by_token':
+            cursor.execute(
+                """SELECT * FROM message WHERE token=%s;""", (token,)
+            )
+            return cursor.fetchall()
 
+    
     except Exception as ex:
         print('Error occured', ex)
     
@@ -165,6 +173,60 @@ def postgres_messages(method, user_id_to=None, user_id_from=None, text=None):
         if psql:
             cursor.close()
             psql.close()
+
+
+
+
+# Func to connect to tokens for messages table
+
+def postgres_chats_tokens(method, user_id_1=None, user_id_2=None):
+    psql = psycopg2.connect(host=os.environ['POST_HOST'], user=os.environ['POST_USER'], password=os.environ['POST_PASSWORD'], database=os.environ['POST_DB'])
+    try:
+        psql.autocommit = True
+        cursor = psql.cursor()
+
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS chat_tokens(id SERIAL PRIMARY KEY, user_id_1 BIGINT NOT NULL, user_id_2 BIGINT NOT NULL, token varchar(50));"""
+        )
+
+        user_1 = user_id_1
+        user_2 = user_id_2
+
+        if user_id_2 < user_id_1:
+            user_1 = user_id_2
+            user_2 = user_id_1
+        else:
+            pass
+
+        if method == 'create_chat_token':
+            cursor.execute(
+                """SELECT * from chat_tokens WHERE user_id_1=%s AND user_id_2=%s;""", (user_1, user_2, )
+            )
+            result = cursor.fetchone()
+            if result == None:
+                cursor.execute(
+                    """INSERT INTO chat_tokens(user_id_1, user_id_2, token) VALUES(%s, %s, %s);""", (user_1, user_2, secrets.token_hex(10))
+                )
+            else:
+                return True
+
+
+        if method == 'get_token':
+            cursor.execute(
+                """SELECT token from chat_tokens WHERE user_id_1=%s AND user_id_2=%s;""", (user_1, user_2, )
+            )
+            return cursor.fetchone()
+
+
+
+
+    except Exception as ex:
+        print('Error occured', ex)
+    
+    finally:
+        if psql:
+            cursor.close()
+            psql.close()            
 
 
 
@@ -225,7 +287,7 @@ def login():
 def index():
     if validate_user() != None:
 
-        return render_template('index.html', users=postgres_profiles('find_all_users'))
+        return render_template('index.html', users=postgres_profiles('find_all_users'), user=postgres_profiles('what_user', id=validate_user()))
 
     return redirect('login.html')
 
@@ -233,30 +295,69 @@ def index():
 
 
 
-# Route to send and view messages
+# # Route to send and view messages
 
-@app.route('/text.html')
-def view():
+# @app.route('/text.html')
+# def view():
+#     if validate_user() != None:
+
+#         sent_messages = postgres_messages('get_sent_messages', user_id_from=validate_user())
+#         received_messages = postgres_messages('get_received_messages', user_id_to=validate_user())
+#         all_messages = sent_messages + received_messages
+#         return render_template('text.html', user=postgres_profiles('what_user', id=validate_user()), messages=do_stuff_with_messages(all_messages))
+
+#     return redirect('/login.html')
+
+
+# @app.route('/send_text', methods=['POST'])
+# def text_send():
+#     if validate_user() != None:
+
+#         to_whom_id = postgres_profiles('get_user_id', request.form['to'])
+#         postgres_messages('send_text', user_id_from=validate_user(), user_id_to=to_whom_id, text=request.form['text'])
+#         return redirect('/text.html')
+
+#     return redirect('/login.html')
+
+
+
+
+# Route to pick user to start chating
+
+@app.route('/pick_user', methods=['POST'])
+def pick_user():
     if validate_user() != None:
 
-        sent_messages = postgres_messages('get_sent_messages', user_id_from=validate_user())
-        received_messages = postgres_messages('get_received_messages', user_id_to=validate_user())
-        all_messages = sent_messages + received_messages
-        return render_template('text.html', user=postgres_profiles('what_user', id=validate_user()), messages=do_stuff_with_messages(all_messages))
+        user_to_id = int(request.form['user_to_id'])
+        user_from_id = validate_user()
+
+        postgres_chats_tokens('create_chat_token', user_id_1=user_from_id, user_id_2=user_to_id)
+        token = postgres_chats_tokens('get_token', user_id_1=user_from_id, user_id_2=user_to_id)
+
+        all_messages = postgres_messages('get_messages_by_token', token=token[0])
+ 
+        return render_template('message.html', user=postgres_profiles('what_user', id=user_from_id), user_to=postgres_profiles('what_user', id=user_to_id), messages=do_stuff_with_messages(all_messages))
 
     return redirect('/login.html')
 
 
-@app.route('/send_text', methods=['POST'])
-def text_send():
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
     if validate_user() != None:
 
-        to_whom_id = postgres_profiles('get_user_id', request.form['to'])
-        postgres_messages('send_text', user_id_from=validate_user(), user_id_to=to_whom_id, text=request.form['text'])
-        return redirect('/text.html')
+        user_from_id = validate_user()
+        user_to_id = postgres_profiles('get_user_id', request.form['to'])
+
+        token = postgres_chats_tokens('get_token', user_id_1=user_from_id, user_id_2=user_to_id[0])
+
+        postgres_messages('send_text', token=token, user_id_to=user_to_id[0], user_id_from=validate_user(), text=request.form['text'])
+
+        all_messages = postgres_messages('get_messages_by_token', token=token[0])
+
+        return render_template('message.html', user=postgres_profiles('what_user', id=user_from_id), user_to=postgres_profiles('what_user', id=user_to_id), messages=do_stuff_with_messages(all_messages))
 
     return redirect('/login.html')
-
 
 
 
